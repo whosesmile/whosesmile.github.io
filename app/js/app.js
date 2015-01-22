3934d1793967b7182bd36dc7cd6484c4aa6d07c6
whosesmile <lovemoon@yeah.net>
2015-01-22 18:11:11 +0800

    change animate

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var panel = $('.simular');
-  var words = '张春苗你老公叫你回家吃饭饭啦';
+  var words = '李二喵你老公叫你回家吃饭饭啦';
 
   var colors = function () {
     var number = function () {
@@ -11,15 +11,17 @@
     return 'rgba(' + number() + ',' + number() + ',' + number() + ',' + (0.5 + Math.random() * 0.5) + ')';
   };
 
+  var count = 0;
+
   function animate() {
-    panel.empty();
     words.split('').forEach(function (letter, i) {
       setTimeout(function () {
-        $('<span class="font">' + letter + '</span>').css('color', colors()).appendTo(panel);
-      }, i * 350);
+        $('<span class="font">' + letter + '</span>').css('color', colors()).css('left', Math.random() * 568).appendTo(panel);
+      }, i * 100);
     });
-
-    setTimeout(animate, words.split('').length * 450);
+    if (++count < 5) {
+      setTimeout(animate, words.split('').length * 100);
+    }
   }
 
   animate();

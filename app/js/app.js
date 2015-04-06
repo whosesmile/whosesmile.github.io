82ec75ff45e35ae30266329ef8adb69ca137ab9e
whosesmile <lovemoon@yeah.net>
2015-04-06 11:29:52 +0800

    add effect

--- app/js/app.js
+++ app/js/app.js
@@ -2,26 +2,53 @@
   'use strict';
 
   var panel = $('.simular');
-  var words = '李二喵，你老公要回家吃饭饭饭';
+  var words = '李二喵不生气';
+
+  var random = function (min, max) {
+    return min + parseInt(Math.random() * (max - min));
+  };
 
   var colors = function () {
-    var number = function () {
-      return parseInt(Math.random() * 255)
-    };
-    return 'rgba(' + number() + ',' + number() + ',' + number() + ',' + (0.5 + Math.random() * 0.5) + ')';
+    return 'rgba(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 5, 1) + ')';
+  };
+
+  var speed = function (t) {
+    return 1 * t/100;
+  };
+
+  var entity = function (letter) {
+    var box = $('<span class="font" />').append(letter);
+    box.css({
+      top: 0,
+      left: random(0, 320)
+    });
+
+    var i = 0;
+    setInterval(function () {
+      ++i;
+      var top = speed(i * 40);
+      if (top > 568) {
+        i = 0;
+        top = -30;
+        box.css('left', random(0, 320));
+      }
+      box.css('top', top);
+    }, 40)
+    return box;
   };
 
   var count = 0;
 
   function animate() {
-    words.split('').forEach(function (letter, i) {
-      setTimeout(function () {
-        $('<span class="font">' + letter + '</span>').css('color', colors()).css('left', Math.random() * 568).appendTo(panel);
-      }, i * 100);
-    });
-    if (++count < 5) {
-      setTimeout(animate, words.split('').length * 100);
-    }
+    entity('你').css('color', colors()).appendTo(panel);
+    // words.split('').forEach(function (letter, i) {
+    //   setTimeout(function () {
+    //     entity(letter).css('color', colors()).appendTo(panel);
+    //   }, i * 100);
+    // });
+    // if (++count < 5) {
+    //   setTimeout(animate, words.split('').length * 100);
+    // }
   }
 
   animate();

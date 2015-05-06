667ffd8f8a6e5f4552323e78432b2ff08bf9e0f2
whosesmile <lovemoon@yeah.net>
2015-05-06 13:15:09 +0800

    optmize image load bug

--- conf/js/product.js
+++ conf/js/product.js
@@ -1,13 +1,10 @@
 $(function () {
   var offset = $('#header').height();
-  var height = $('#slider').height() / $('#slider img').size();
   var buttons = $('#navButtons li');
-
   buttons.each(function (index, button) {
     $(button).on('click', function (e) {
-      // $('#slider').css('transform', 'translate(0px, -' + index * height + 'px)');
       $('html,body').animate({
-        scrollTop: offset + index * height
+        scrollTop: offset + index * $('#slider').height() / $('#slider img').size()
       });
       buttons.removeClass('active').eq(index).addClass('active');
     });

31f5ec7dc8a938c238088dcbefeccc320d36f23d
whosesmile <lovemoon@yeah.net>
2015-05-18 16:54:52 +0800

    init conf weixin project

---
+++ weixin/js/home.js
@@ -0,0 +1,2 @@
+/*! mobile - v0.0.1 - * Copyright (c) 2015 lovemoon@yeah.net; Licensed GPLv2 */
+!function(){$(function(){$("form").on("click",function(a){var b='<div class="cornertips"><i class="icon icon-arrow-corner"></i><span>请点击右上角按钮，<br />选择浏览器打开，否则无法进入会议！</span><i class="icon icon-pointer"></i></div>';return masklayer.show(b),!1})})}();

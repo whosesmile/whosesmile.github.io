3f1f917b639f703834550af1b877442880a5f6fc
whosesmile <lovemoon@yeah.net>
2015-05-24 23:38:30 +0800

    qianding official

---
+++ official/js/base.js
@@ -0,0 +1,8 @@
+// 滚动
+$(function () {
+  $('#qianding').fullpage({
+    // sectionsColor: ['#ffffff', '#fc4c35', '#f5f6fa', '#03132a', '#ffffff', '#e8fbff', '#ffffff', '#ffffff'],
+    // anchors: ['s1', 's2', 's3', 's4', 's5', 's6', 's6', 's7'],
+    scrollingSpeed: 700
+  });
+});

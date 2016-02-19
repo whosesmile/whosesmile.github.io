530858dfeb43f8034784e8d12c709f6b6c1599d6
whosesmile <lovemoon@yeah.net>
2016-02-19 14:23:35 +0800

    optmize code

--- conf/js/solution.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $('.tabview .tab').on('click', function (e) {
-    var tab = $(this);
-    var name = tab.data('view');
-    tab.addClass('active').siblings('.tab').removeClass('active');
-    $('.tabview .view.' + name).addClass('active').siblings('.view').removeClass('active');
-    return false;
-  });
-});
